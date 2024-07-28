d3884ab3923a31e2ef6351f2d3b25699d94a3281
hugobor <hugoborgescarvalho@gmail.com>
2024-07-28 01:08:38 -0300

    Projeto inicial

---
+++ blabu-proju-spring/src/main/java/com/github/hugobor/blabu_proju_spring/BlabuProjuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.hugobor.blabu_proju_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabuProjuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlabuProjuSpringApplication.class, args);
+	}
+
+}
---
+++ blabu-proju-spring/src/test/java/com/github/hugobor/blabu_proju_spring/BlabuProjuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hugobor.blabu_proju_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlabuProjuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
