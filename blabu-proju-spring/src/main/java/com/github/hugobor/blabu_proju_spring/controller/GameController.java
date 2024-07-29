6c72eb501ba46db587eeb05067e0a427c59ea03a
hugobor <hugoborgescarvalho@gmail.com>
2024-07-29 00:42:21 -0300

    Método PUT de `Game`

--- blabu-proju-spring/src/main/java/com/github/hugobor/blabu_proju_spring/controller/GameController.java
+++ blabu-proju-spring/src/main/java/com/github/hugobor/blabu_proju_spring/controller/GameController.java
@@ -1,13 +1,18 @@
 package com.github.hugobor.blabu_proju_spring.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.hugobor.blabu_proju_spring.model.Game;
 import com.github.hugobor.blabu_proju_spring.model.GameRepository;
@@ -31,7 +36,7 @@ public class GameController {
 	}
 	
 	@GetMapping("/{id}")
-	public Optional<Game> games(@PathVariable Long id) {
+	public Optional<Game> games(@PathVariable("id") Long id) {
 		return gameRepository.findById(id);
 	}
 	
@@ -75,4 +80,32 @@ public class GameController {
 		return List.of(PlayState.values()).stream().map(state -> state.toString().toLowerCase()).toList();
 	}
 	
+	
+	@PostMapping
+	public ResponseEntity<Game> createGame(@RequestBody Game game) {
+		var plat = game.getPlatform();
+		if (plat != null) {
+			if (plat.getSimpleName() != null) {
+			platformRepository
+				.findBySimpleName(plat.getSimpleName())
+				.ifPresent(foundPlat -> game.setPlatform(foundPlat));
+			} else if (plat.getId() != null) {
+				platformRepository
+					.findById(plat.getId())
+					.ifPresent(foundPlat -> game.setPlatform(foundPlat));
+			}
+			platformRepository.save(game.getPlatform());
+		}
+		
+				
+		var createdGame = gameRepository.save(game);
+		
+		URI createdURI = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(createdGame.getId())
+				.toUri();
+		
+		return ResponseEntity.created(createdURI).body(createdGame);
+	}
 }
